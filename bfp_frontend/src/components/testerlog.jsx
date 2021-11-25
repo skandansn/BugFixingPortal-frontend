3060e244a094198c7c24772a067bd1d20d95c984
Ranjit Chandramohanan <ranjit.c2000@gmail.com>
2021-11-26 01:57:05 +0530

    Added Tester Log

--- bfp_frontend/src/components/testerlog.jsx
+++ bfp_frontend/src/components/testerlog.jsx
@@ -6,24 +6,21 @@ import Loader from "../routes/Loader";
 import SideNavbar from "./sidenavbar";
 
 
-//card component to get solutions from api and display them
-
-const ViewSolutions = () => {
-  const [solutions, setSolutions] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const {id, id2} = useParams()
-  const history = useHistory();
-  useEffect(() => {
-    axios.get(`http://localhost:8080/projects/${id}/issues/${id2}/solutions`,{headers: {'Authorization': 'Bearer '+localStorage.getItem('token')}})
-    .then(res => {
-        setSolutions(res.data);
-        console.log(solutions);
-        setLoading(false);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-  }, []);
+const ViewTesters = () =>{
+    const [testers, setTesters] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const id = useParams()
+    const history = useHistory();
+    useEffect(() => {
+        axios.get(`http://localhost:8080/projects/${id.id}/testerlog`,{headers: {'Authorization': 'Bearer '+localStorage.getItem('token')}})
+        .then(res => {
+            setTesters(res.data);
+            setLoading(false);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }, []);
 
   return (
     <div>
@@ -33,30 +30,29 @@ const ViewSolutions = () => {
                         <SideNavbar />
                         <div className="grid-chid-b bg-danger " id="desc-container">
                             <div>
-                            <Link to={`/createProject/s${id2}p${id}`} className="btn btn-success">Add Solution</Link>
                             <table className="table table-striped bg-light mar-top">
               <thead>
                   <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">Title</th>
-                      <th scope="col">Description</th>
-                      <th scope="col">Files</th>
-                      <th scope="col">Created By</th>
+                      <th scope="col">ID</th>
+                      <th scope="col">Handle</th>
+                      <th scope="col">Projects Collaborated</th>
+                      <th scope="col">Issues Reported in this Project</th>
+                      <th scope="col">Solutions Provided in this Project</th>
+
 
                   </tr>
               </thead>
               <tbody>
                   {/* map through issues and display them */}
-                  {solutions.map((solution, index) => {
+                  {testers.map((tester, index) => {
                       return (
                           <tr key={index}>
                               <th scope="row">{index + 1}</th>
-                              <td>{solution.solutionTitle}</td>
-                              <td>{solution.solutionDesc}</td>
-                              <td>
-                                  <button className="btn-warning btn-sm"><a href={solution.solutionFiles} style={{"text-decoration": "none","color":"black"}}>View File</a></button>
-                                  </td>
-                              <td>{solution.user.userHandle}</td>
+                              <td>{tester.testerHandle}</td>
+                              <td>{tester.testerProjects}</td>
+                              <td>{tester.testerIssues}</td>
+                              <td>{tester.testerSolutions}</td>
+                              <td>{tester.testerRating}</td>
                           </tr>
                       );
                   })}
@@ -73,4 +69,4 @@ const ViewSolutions = () => {
 
   );
 };
-export default ViewSolutions;
+export default ViewTesters;
